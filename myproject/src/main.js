f5b8e63212c0e266d6a34e537444470314916a5b
Bi-Ma-GOoOD <xphlayyi@gmail.com>
2025-04-09 15:04:02 +0700

    I_forgot_feature_branch_TT

---
+++ myproject/src/main.js
@@ -0,0 +1,51 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+// สร้างกล้องในการเคลื่อนที่ในแกน Z เหมือนตอนทีเรียน ICG
+camera.position.setZ(30);
+// วาดภาพ
+renderer.render(scene, camera);
+// Geometry: the {x, y, z} points that makeup a shape
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+
+const torus = new THREE.Mesh(geometry, material);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(10, 10, 10);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+
+scene.add(torus);
+scene.add(pointLight, ambientLight);
+scene.add(lightHelper, gridHelper);
+
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
